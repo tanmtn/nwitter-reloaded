2efaf0b0fc0a33b5350070d6a4261a35c2189543
tanmtn <tanmtni@gmail.com>
2023-09-11 14:57:02 +0900

    1.3 Firebase Project

---
+++ src/firebase.ts
@@ -0,0 +1,14 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+    apiKey: 'AIzaSyBbh21p9cymXq0M-mE3vaTg28S8BGG873c',
+    authDomain: 'nwitter-reloaded-a09d1.firebaseapp.com',
+    projectId: 'nwitter-reloaded-a09d1',
+    storageBucket: 'nwitter-reloaded-a09d1.appspot.com',
+    messagingSenderId: '95681970091',
+    appId: '1:95681970091:web:8e6498a6b5ee398b4cdcee',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
